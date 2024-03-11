22a12fceaef0e0c9bab92d0cd47c75b8c2a0065c
Ana <ana3.goncalves@live.uwe.ac.uk>
2024-03-11 13:26:21 +0000

    received

--- client_receiver.cpp
+++ client_receiver.cpp
@@ -5,12 +5,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 
 int main() {
     // set client IP address
     uwe::set_ipaddr("192.168.1.8");
 
-    const int server_port = 8877;
+    int server_port = 8877;
 
     struct sockaddr_in server_address; // data structure for storing IPv4 or IPv6 addresses
 	memset(&server_address, 0, sizeof(server_address));
@@ -20,15 +21,14 @@ int main() {
 	server_address.sin_port = htons(server_port);
 
     // creates binary representation of server name and stores it as sin_addr
-	inet_pton(AF_INET, uwe::set_ipaddr().c_str(), &server_address.sin_addr);
+	inet_pton(AF_INET, uwe::get_ipaddr().c_str(), &server_address.sin_addr);
 
     uwe::socket sock{AF_INET, SOCK_DGRAM, 0};
 
-    sock.bind((struct sockaddr *)&client_address, sizeof(client_address));
+    sock.bind((struct sockaddr *)&server_address, sizeof(server_address));
 
     struct sockaddr_in client_address;
     size_t client_address_len = 0;
-    ssize_t socket::recvfrom(void * buf, size_t len, int flags, sockaddr * src_addr, size_t *addrlen);
 
     char buffer[1024];
     int len = sock.recvfrom(buffer, sizeof(buffer),0, (struct sockaddr *)& client_address, &client_address_len);
